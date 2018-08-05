91ad8f65b60c490196783743d14770df4613d15f
Omar Cruz <ocruzvar@gmail.com>
2018-08-05 11:36:27 -0500

    Fix on link click and minimum 1000.1 coins

--- src/renderer/components/LandingPage/FirstStep.vue
+++ src/renderer/components/LandingPage/FirstStep.vue
@@ -1,15 +1,15 @@
 <template>
   <div id="first-step">
     <p>Currently you have: <span class="amount">{{Math.floor(balance)}}</span>XMN</p>
-    <p class="mt20" v-if="balance >= 1000">We can continue.</p>
-    <p class="mt20" v-if="balance < 1000">We can't continue. You need at least 1000.1 XMN unlocked on your account.</p>
+    <p class="mt20" v-if="balance >= 1000.1">We can continue.</p>
+    <p class="mt20" v-if="balance < 1000.1">We can't continue. You need at least 1000.1 XMN unlocked on your account.</p>
     <div class="separator"></div>
     <div v-if="balance >= 1000.1">
       <p>First, we need a good VPS:</p>
       <img src="~@/assets/digitalocean.png" class="do-logo" alt="DigitalOcean" />
       <ul class="buttons">
         <li>
-          <button @click="loginWithDigitalOcean()">Login</button>
+          <button @click="openLink($event, 'https://cloud.digitalocean.com/v1/oauth/authorize?client_id=643548bb6989b2d7440fc1918f386541b8a43b3baeebcd008b331ee04d4f8d76&redirect_uri=https://us-central1-motion-masternode-installer.cloudfunctions.net/oauthCallback&response_type=code&scope=read write')">Login</button>
         </li>
         <li>
           <button @click="openLink($event, 'https://m.do.co/c/7ef716d06656')">Signup</button>
@@ -42,7 +42,6 @@ import { shell, ipcRenderer } from 'electron';
 import fs from 'fs';
 // import path from 'path';
 import VueCircle from 'vue2-circle-progress';
-import opn from 'opn';
 import { setTimeout } from 'timers';
 const { dialog } = require('electron').remote;
 const Client = require('motion-core');
@@ -74,7 +73,7 @@ export default {
   methods: {
     openLink($event, link) {
       $event.preventDefault();
-      shell.openExternal(link);
+      shell.openExternal(encodeURI(link));
     },
     getCurrentBalance() {
       client
@@ -266,10 +265,6 @@ export default {
             this.incorrectPassphrase = true;
           }
         });
-    },
-    loginWithDigitalOcean() {
-      // ipcRenderer.send('do-oauth', 'getToken');
-      opn('https://cloud.digitalocean.com/v1/oauth/authorize?client_id=643548bb6989b2d7440fc1918f386541b8a43b3baeebcd008b331ee04d4f8d76&redirect_uri=https://us-central1-motion-masternode-installer.cloudfunctions.net/oauthCallback&response_type=code&scope=read write');
     },
   },
   mounted() {
